4f8f56e2d520eaef506947fcdfa3490c16923473
Adam R Moss <adamrmoss@gmail.com>
2022-08-25 07:19:58 -0400

    First experiments with template

---
+++ FirstXboxGame.cs
@@ -0,0 +1,74 @@
+﻿using Windows.Foundation;
+using Windows.Graphics.Display;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FirstXboxGame
+{
+    public class FirstXboxGame : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        public FirstXboxGame()
+        {
+            this.graphics = new GraphicsDeviceManager(this);
+            this.Content.RootDirectory = "Content";
+            this.IsMouseVisible = false;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+
+            var view = DisplayInformation.GetForCurrentView();
+
+            var resolution = new Size(view.ScreenWidthInRawPixels, view.ScreenHeightInRawPixels);
+
+            var bounds = new Size(resolution.Width, resolution.Height);
+
+
+            var viewportBounds        = this.GraphicsDevice.Viewport.Bounds;
+            var viewportTitleSafeArea = this.GraphicsDevice.Viewport.TitleSafeArea;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            this.spriteBatch = new SpriteBatch(this.GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            var pressingBackButton = GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed;
+            var pressingBButton = GamePad.GetState(PlayerIndex.One).Buttons.B == ButtonState.Pressed;
+            var pressingEscapeKey  = Keyboard.GetState().IsKeyDown(Keys.Escape);
+
+            if (pressingBackButton || pressingBButton || pressingEscapeKey)
+            {
+                this.Exit();
+            }
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            this.GraphicsDevice.Clear(Color.Yellow);
+
+            //var viewportBounds        = this.GraphicsDevice.Viewport.Bounds;
+            //var viewportTitleSafeArea = this.GraphicsDevice.Viewport.TitleSafeArea;
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ GamePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Windows.Foundation;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace FirstXboxGame
+{
+    public sealed partial class GamePage : Page
+    {
+        readonly FirstXboxGame game;
+
+        public GamePage()
+        {
+            MaximizeWindowOnLoad();
+            this.InitializeComponent();
+
+            // Create the game.
+            var launchArguments = string.Empty;
+            this.game = MonoGame.Framework.XamlGame<FirstXboxGame>.Create(launchArguments, Window.Current.CoreWindow, swapChainPanel);
+        }
+
+        private static void MaximizeWindowOnLoad()
+        {
+            var view = DisplayInformation.GetForCurrentView();
+
+            var resolution = new Size(view.ScreenWidthInRawPixels, view.ScreenHeightInRawPixels);
+
+            var scale = view.ResolutionScale == ResolutionScale.Invalid ? 1 : view.RawPixelsPerViewPixel;
+            var bounds = new Size(resolution.Width / scale, resolution.Height / scale);
+
+            ApplicationView.PreferredLaunchViewSize = new Size(bounds.Width, bounds.Height);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+    }
+}
